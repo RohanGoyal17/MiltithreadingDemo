da8991c5a04c42884b80eb0c7363040265050dc7
Rohan <rohan17091999@gmail.com>
2021-02-04 21:56:25 +0530

    sync example

--- src/com/rohan/Main.java
+++ src/com/rohan/Main.java
@@ -1,32 +1,46 @@
 package com.rohan;
 
+
 class Worker extends Thread {
 
     private Integer a;
     private Integer b;
+    private Integer x = 1;
 
     public Worker(Integer a, Integer b) {
         this.a = a;
         this.b = b;
     }
 
+    public Integer getX() {
+        return x;
+    }
+
     @Override
     public void run() {
         //for (int i = 0; i <= 5; i++) {
             System.out.println(Thread.currentThread().getName() + ": " + (a + b));
+            x = a+b;
         //}
     }
 
 }
 
 public class Main {
-
     public static void main(String[] args) {
-        Thread t1 = new Worker(1,2);
-        Thread t2 = new Worker(3,4);
-        Thread t3 = new Worker(10,20);
+        Worker t1 = new Worker(1,2);
+        Worker t2 = new Worker(3,4);
+        Worker t3 = new Worker(10,20);
         t1.start();
         t2.start();
         t3.start();
+        //for(int i = 0; i< 100; i++)
+        System.out.println(t1.getX());
+        try{
+            t1.join();
+        }catch (Exception e){
+            System.out.println("Joining error");
+        }
+        System.out.println(t1.getX());
     }
 }
